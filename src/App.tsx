145c562154749247e8f5c0f12e2b321486c5a9b5
Andrew <andrewhu1997@gmail.com>
2025-08-17 18:37:30 -0400

    load through hashes to load pages independently at pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './Pages/Main';
 import PersonalityPredictorPage from './Pages/PersonalityPredictorPage';
 
-const router = createBrowserRouter([
-  {
-    path: '/personal-site',
-    element: <Main />,  
-  },
-  {
-    path: '/personality-predictor',
-    element: <PersonalityPredictorPage />,  
-  },
-]);
-
 const App = () => {
   return (
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/personal-site" element={<Main />} />
+        <Route path="/personality-predictor" element={<PersonalityPredictorPage />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
-
